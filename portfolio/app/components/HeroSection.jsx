782eedc1941c7b8d9c86c4e9acb836bce26b89bd
kitsosenwedi <boyboykitso@gmail.com>
2024-01-11 13:56:45 +0200

    herosection run time error

---
+++ portfolio/app/components/HeroSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h1 className="text-white mb-4 text-6xl font-extrabold">Hello, I'm Kitso Senwedi</h1>
+        <p className="text-[#ADB7BE]">
+        Enthusiastic and highly motivated freelance web developer with a strong foundation in Information
+Technology and a passion for creating innovative and user-friendly websites. A recent graduate
+with a Bachelor's degree in Information Technology, I bring fresh ideas and a solid technical skill
+set to every project. Proficient in HTML5, CSS3, JavaScript, and front-end frameworks like
+Bootstrap.
+        </p>
+    </section>
+  );
+};
+
+export default HeroSection
