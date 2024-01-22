fbf308d6ba399b57d851888094dbcb2eb06d6335
kitsosenwedi <boyboykitso@gmail.com>
2024-01-22 13:13:59 +0200

    updating general information

--- portfolio/src/app/components/AboutSection.jsx
+++ portfolio/src/app/components/AboutSection.jsx
@@ -57,12 +57,13 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
+            I am a web developer with a passion for creating
             interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
+            working with JavaScript, Tailwind, Bootstrap React, Node.js, HTML, CSS, and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            I am excited to work with others to create amazing applications. A recent graduate
+with a Bachelor's degree in Information Technology, I bring fresh ideas and a solid technical skill
+set to every project.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
